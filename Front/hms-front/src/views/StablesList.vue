927d3de89ed40a2bfb283407815f32398af55257
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-01-19 00:17:05 +0100

    :bug: forget to add the new file

---
+++ Front/hms-front/src/views/StablesList.vue
@@ -0,0 +1,21 @@
+<template lang="fr">
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name : "StablesList",
+    data() {
+        return {
+            selectedStables : undefined,
+            showMore : false,
+            message : "nothing to show",
+            stables : []
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
