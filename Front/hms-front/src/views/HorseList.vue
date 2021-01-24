0fac33c2d75e958c7f92604aaf734df9c5c75bd0
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-01-24 02:01:52 +0100

    :construction: work on assign to stable card, And i have some pproblem to send the data i want -> took some time to manipulate a drodown menu unsuccesfull for now
    actually i want to send the id of the object i am holding, however the dropdpopwn select just keep a string and not the full object, so i lost the id and i have to send the name instead of an ID and it is roblematic because if the ersone has the same name ... I cannotn corectly differenciate them ^^

--- Front/hms-front/src/views/HorseList.vue
+++ Front/hms-front/src/views/HorseList.vue
@@ -2,90 +2,137 @@
   <div>
     <div class="container">
       <div class="columns">
-  <div class="column is-two-fifths">
-    <h1>Liste des chevaux</h1>
-    <ul class="horse-list">
-      <li v-for="horse in horses" :key="horse.id" class="menu-list horse-elem-in-list">
-        <a class="list-item" @click="selectedHorse = horse"
-            :class="{ 'is-active': selectedHorse === horse}">
-          <span class="title">
-              {{ horse.horse_name }}
-            <button class="button is-primary is-light" v-on:click="addMedicIntervention(horse)" v-show="selectedHorse === horse">+</button>
-            <button class="button is-primary is-link is-light" v-on:click="assignToNewStable(horse)" v-show="selectedHorse === horse">-></button>
-          </span>
-        </a>
-      </li>
-    </ul>
-    <p v-if="message">{{ message }}</p>
-    <div v-if="selectedHorse">
-      <div class="field">{{ selectedHorse.horse_name | fulli }}</div>
-      <label for="show" class="checkbox">
-        <input type="checkbox" class="is-primary" id="show" v-model="showMore" />
-      </label>
-      <!-- show more info fields -->
-      <div v-show="showMore">
-        <div class="field">Size : {{ selectedHorse.size }}</div>
-        <div class="field">Coat color : {{ selectedHorse.colorname }}</div>
-        <div class="field">Last name Owner : {{ selectedHorse.lastname }}</div>
-      </div>
-    </div>
-    <!-- button to add a horse -->
-    <div>
-      <button class="button is-primary " v-on:click="showNewCard" v-show="!newHorse">+</button>
-    </div>
-    <!-- to add new horse -->
-    <div v-show="newHorse">
-        {{ fullInfo }}
-        <div class="field">
-          <div class="control">
-            <input class="input is-primary" id="name" type="text" placeholder="new horse name" required minlength="2" maxlength="255" v-model="newHorseCard.horse_name">
-            <div class="select">
-              <select v-model="ownerSelected">
-                <option v-for="owner in owners" :key="owner.id" >{{ ownerNameFormated(owner) }}</option>
-              </select>
-            </div>
-            <div class="select">
-              <select v-model="sexSelected">
-                <option v-for="sx in sexs" :key="sx.id" >{{ sx.sex_name }}</option>
-              </select>
-            </div>
-            <div class="select">
-              <select v-model="coatSelected">
-                <option v-for="ct in coats" :key="ct.id" >{{ ct.color_name }}</option>
-              </select>
+        <div class="column is-two-fifths">
+          <h1>Liste des chevaux</h1>
+          <ul class="horse-list">
+            <li v-for="horse in horses" :key="horse.id" class="menu-list horse-elem-in-list">
+              <a
+                class="list-item"
+                @click="selectedHorse = horse"
+                :class="{ 'is-active': selectedHorse === horse}"
+              >
+                <span class="title">
+                  {{ horse.horse_name }}
+                  <button
+                    class="button is-primary is-light"
+                    v-on:click="addMedicIntervention(horse)"
+                    v-show="selectedHorse === horse"
+                  >+</button>
+                  <button
+                    class="button is-primary is-link is-light"
+                    v-on:click="assignToNewStable(horse)"
+                    v-show="selectedHorse === horse"
+                  >-></button>
+                </span>
+              </a>
+            </li>
+          </ul>
+          <p v-if="message">{{ message }}</p>
+          <div v-if="selectedHorse">
+            <div class="field">{{ selectedHorse.horse_name | fulli }}</div>
+            <label for="show" class="checkbox">
+              <input type="checkbox" class="is-primary" id="show" v-model="showMore" />
+            </label>
+            <!-- show more info fields -->
+            <div v-show="showMore">
+              <div class="field">Size : {{ selectedHorse.size }}</div>
+              <div class="field">Coat color : {{ selectedHorse.colorname }}</div>
+              <div class="field">Last name Owner : {{ selectedHorse.lastname }}</div>
             </div>
           </div>
+          <!-- button to add a horse -->
+          <div>
+            <button class="button is-primary" v-on:click="showNewCard" v-show="!newHorse">+</button>
+          </div>
+          <!-- to add new horse -->
+          <div v-show="newHorse">
+            {{ fullInfo }}
+            <div class="field">
+              <div class="control">
+                <input
+                  class="input is-primary"
+                  id="name"
+                  type="text"
+                  placeholder="new horse name"
+                  required
+                  minlength="2"
+                  maxlength="255"
+                  v-model="newHorseCard.horse_name"
+                />
+                <div class="select">
+                  <select v-model="ownerSelected">
+                    <option v-for="owner in owners" :key="owner.id">{{ ownerNameFormated(owner) }}</option>
+                  </select>
+                </div>
+                <div class="select">
+                  <select v-model="sexSelected">
+                    <option v-for="sx in sexs" :key="sx.id">{{ sx.sex_name }}</option>
+                  </select>
+                </div>
+                <div class="select">
+                  <select v-model="coatSelected">
+                    <option v-for="ct in coats" :key="ct.id">{{ ct.color_name }}</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <p>{{ newHorseCard.name }}</p>
+            <button class="button is-primary is-rounded" v-on:click="ValidateNewwHorse">Validate</button>
+          </div>
         </div>
-        <p>{{ newHorseCard.name }} </p>
-        <button class="button is-primary is-rounded" v-on:click="ValidateNewwHorse" >Validate</button>
-    </div>
-    
-  </div>
-    <div class="column">
-    Second column
-    <!-- to add medic report -->
-    <div v-show="showMedicCard">
-      <p>Add a medic report</p>
-      <div class="field">
-        <div class="control">
-          <div class="select">
-              <select v-model="vetSelected">
-                <option v-for="vet in veterinarian" :key="vet.id" >{{ ownerNameFormated(vet) }}</option>
-              </select>
+        <div class="column">
+          Second column
+          <!-- to add medic report -->
+          <div v-show="showMedicCard">
+            <p>Add a medic report</p>
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select v-model="vetSelected">
+                    <option v-for="vet in veterinarian" :key="vet.id">{{ ownerNameFormated(vet) }}</option>
+                  </select>
+                </div>
+                <div class="select">
+                  <select v-model="medicInterventionSelected">
+                    <option
+                      v-for="typeMed in typeMedic"
+                      :key="typeMed.id"
+                    >{{ typeMed.type_medic_name }}</option>
+                  </select>
+                </div>
+                <textarea
+                  class="textarea"
+                  placeholder="Note about the medical intervention"
+                  v-model="noteMedic"
+                ></textarea>
+                <button
+                  class="button is-primary is-rounded"
+                  :disabled="checkMedicInfo"
+                  v-on:click="ValidateNewwIntervention"
+                >Validate Medic Intervention</button>
+                <button class="button is-danger" v-on:click="showMedicCard = !showMedicCard">Cancel</button>
+              </div>
             </div>
-            <div class="select">
-              <select v-model="medicInterventionSelected">
-                <option v-for="typeMed in typeMedic" :key="typeMed.id" >{{ typeMed.type_medic_name }}</option>
-              </select>
+          </div>
+          <!-- to assign to new stable -->
+          <div v-show="showStableCard">
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select v-model="stableSelected">
+                    <option v-for="stb in stables" :key="stb.id">{{ stb.stable_name }}</option>
+                  </select>
+                </div>
+              </div>
             </div>
-            <textarea class="textarea" placeholder="Note about the medical intervention" v-model="noteMedic"></textarea>
-            <button class="button is-primary is-rounded" :disabled="checkMedicInfo" v-on:click="ValidateNewwIntervention">Validate Medic Intervention</button>
-            <button class="button is-danger " v-on:click="showMedicCard = !showMedicCard">Cancel</button>
 
+            <button
+              class="button is-primary is-rounded"
+              v-on:click="validNewStableAssigned"
+            >Validate new Stable</button>
+            <button class="button is-danger" v-on:click="showStableCard = !showStableCard">Cancel</button>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
       </div>
     </div>
   </div>
@@ -164,7 +211,7 @@
   }
 ];*/
 
-import * as axios from 'axios';
+import * as axios from "axios";
 const apiAddr = "http://localhost:5008";
 export default {
   name: "HorseList",
@@ -174,6 +221,7 @@ export default {
     return {
       selectedHorse: undefined,
       ownerSelected: undefined,
+      stableSelected: "",
       sexSelected: undefined,
       coatSelected: undefined,
       vetSelected: undefined,
@@ -181,7 +229,8 @@ export default {
       noteMedic: "",
       showMore: false,
       showMedicCard: false,
-      showStableCarde: false,
+      showStableCard: false,
+      dropdownMenuOneActivated: false,
       message: "horse list",
       newHorse: false,
       horses: [],
@@ -193,29 +242,33 @@ export default {
       typeMedic: [],
       counter: 0,
       newHorseCard: {
-            horse_name: null,
-            size: null,
-            coat: null,
-            sex: null,
-            owner: null
+        horse_name: null,
+        size: null,
+        coat: null,
+        sex: null,
+        owner: null
       }
     };
   },
   filters: {
-        fulli: function(str) {
-            return `Le cheval sélectionné est : ${str}`;
-        }
-    },
+    fulli: function(str) {
+      return `Le cheval sélectionné est : ${str}`;
+    }
+  },
   computed: {
     fullInfo() {
       if (this.newHorseCard.horse_name === null) {
-        return "ajout d'un nouveau cheval"
+        return "ajout d'un nouveau cheval";
       } else {
         return `Le cheval à ajouter est : ${this.newHorseCard.horse_name}`;
       }
     },
     checkMedicInfo() {
-      if (this.vetSelected != undefined && this.medicInterventionSelected != undefined && this.noteMedic != "") {
+      if (
+        this.vetSelected != undefined &&
+        this.medicInterventionSelected != undefined &&
+        this.noteMedic != ""
+      ) {
         return false;
       } else {
         return true;
@@ -229,47 +282,68 @@ export default {
     addMedicIntervention: function(hrs) {
       this.loadMedicData();
       console.log("pour l'ajout d'une intervention medical"),
-      this.showMedicCard = true;
+        (this.showMedicCard = true);
       console.log(hrs.horse_name);
-
     },
     assignToNewStable: function(hrs) {
-      this.loadStables();
       console.log("assignation d'une nouvelle ecurie"),
-      this.showStableCarde = true;
-      console.log(hrs.horse_name);
-
+        (this.showStableCard = true);
+      if (hrs.id_stables == null) {
+        this.stableSelected = "";
+      } else {
+        this.stables.forEach(element => {
+          if (element.stable_id == hrs.id_stables) {
+            this.stableSelected = element;
+          }
+        });
+      }
+    },
+    validNewStableAssigned: function() {
+      //a refacto : le droppdown ne donne que le nom
+      //il faut que je fasse un menu pour garder l'id
+      var id = 0;
+      console.log("validation de l'assignation d'une nouvelle ecurie");
+      console.log(this.stableSelected);
+      this.stables.forEach(element => {
+        if (element.stable_name == this.stableSelected) {
+          id = element.stable_id;
+        }
+      });
+      console.log(this.selectedHorse);
+      this.postMoveHorse(this.selectedHorse.horse_id, id)
+      this.stableSelected = "";
+      this.showStableCard = false;
     },
     goToShowMore: function(selectedHorseToShowMore) {
-          console.log("we are going to show more information from");
-          console.log(selectedHorseToShowMore.horse_name)
-      },
-    showNewCard: function () {
+      console.log("we are going to show more information from");
+      console.log(selectedHorseToShowMore.horse_name);
+    },
+    showNewCard: function() {
       this.newHorse = true;
       this.loadOtherData();
       console.log("j'ai appuyé sur le bouton");
     },
-    ownerNameFormated: function (ownr) {
-      return `${ownr.lastname}  ${ownr.firstname}`
+    ownerNameFormated: function(ownr) {
+      return `${ownr.lastname}  ${ownr.firstname}`;
     },
-    greet: function (event) {
+    greet: function(event) {
       // `this` fait référence à l'instance de Vue à l'intérieur de `methods`
-      alert('Bonjour ' + this.name + ' !')
+      alert("Bonjour " + this.name + " !");
       // `event` est l'évènement natif du DOM
       if (event) {
-        alert(event.target.tagName)
+        alert(event.target.tagName);
       }
     },
     ValidateNewwHorse: function() {
-        console.log("je vais valider un nouveau cheval");
-        console.log(this.newHorseCard.horse_name);
-        console.log(this.ownerSelected);
-        console.log(this.sexSelected);
-        console.log(this.coatSelected);
-        this.newHorseCard.sex = this.sexSelected;
-        this.newHorseCard.coat = this.coatSelected;
-        //this.newHorseCard.o
-        this.postHorse(this.newHorseCard);
+      console.log("je vais valider un nouveau cheval");
+      console.log(this.newHorseCard.horse_name);
+      console.log(this.ownerSelected);
+      console.log(this.sexSelected);
+      console.log(this.coatSelected);
+      this.newHorseCard.sex = this.sexSelected;
+      this.newHorseCard.coat = this.coatSelected;
+      //this.newHorseCard.o
+      this.postHorse(this.newHorseCard);
     },
     ValidateNewwIntervention: function() {
       //TODO : faire la requete
@@ -278,10 +352,11 @@ export default {
       console.log(this.medicInterventionSelected);
       console.log(this.noteMedic);
       console.log(this.selectedHorse);
+
     },
     async getHorses() {
       this.horses = [];
-      const res = await axios.get(`${apiAddr}/horses`)
+      const res = await axios.get(`${apiAddr}/horses`);
       const dataReceived = res.data;
       console.log("les données recu :");
       console.log(dataReceived);
@@ -327,6 +402,18 @@ export default {
         this.loadHorses();
       }
     },
+    async postMoveHorse(hrsid, stableid) {
+      console.log(`le horse id : ${hrsid} et le stable id = ${stableid}`);
+      const res = await axios.post(`${apiAddr}/horses/move`, {"stable_id": stableid, "horse_id":hrsid});
+      console.log(res);
+      this.loadHorses();
+    },
+    async posttMedicReport(vetName, typeName, report, horse) {
+      console.log(vetName)
+      console.log(typeName)
+      console.log(report)
+      console.log(horse)
+    },
     //we need to create another function, if we call directly getHorses() from created() it doesnt work
     async loadHorses() {
       this.horses = [];
@@ -339,17 +426,22 @@ export default {
       this.getOwners();
       this.getSex();
       this.getCoat();
-    }, 
+    },
     async loadMedicData() {
       this.getVet();
       this.getMedicIntervention();
     },
     async loadStables() {
-      this.getStables();
+      if (this.stables) {
+        this.getStables();
+        console.log("fin de getStable");
+        console.log(this.stables);
+      }
     }
   },
   created() {
     this.loadHorses();
+    this.loadStables();
   }
 };
 </script>
